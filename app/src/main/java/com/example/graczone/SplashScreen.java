968581c8d60dc91b8a4f95c1219dc3687fe94ed0
yogeshdrt <yogeshdrt@gmail.com>
2021-05-15 18:01:55 +0530

    joiningComplete/splash

---
+++ app/src/main/java/com/example/graczone/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.example.graczone;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import static java.lang.Thread.sleep;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    sleep(1000);
+                    Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+    }
+}
