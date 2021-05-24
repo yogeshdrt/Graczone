6cb99792977e4c743a9ac9b2087bf05a02f39f7c
Lakhan <lakhan.nishad_cs19@gla.ac.in>
2021-05-24 23:24:32 +0530

    add swipe to delete item

--- app/src/main/java/com/example/graczone/ui/Notification/Notification_Fragment.java
+++ app/src/main/java/com/example/graczone/ui/Notification/Notification_Fragment.java
@@ -1,16 +1,20 @@
 package com.example.graczone.ui.Notification;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.graczone.R;
+import com.google.gson.Gson;
 
 import java.util.ArrayList;
 
@@ -19,8 +23,11 @@ public class Notification_Fragment extends Fragment {
 
     CardView cardView;
 
+    public static final String MyPREFERENCES = "MyPrefs";
+
     RecyclerView recyclerView;
     ArrayList<NotificationModel> notificationModels;
+    NotificationAdapter notificationAdapter;
 
     public Notification_Fragment() {
         // Required empty public constructor
@@ -40,10 +47,41 @@ public class Notification_Fragment extends Fragment {
             notificationModels = (ArrayList<NotificationModel>) bundle.getSerializable("models");
         }
 
-        recyclerView.setAdapter(new NotificationAdapter(notificationModels));
+        notificationAdapter = new NotificationAdapter(notificationModels);
 
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+                //Remove swiped item from list and notify the RecyclerView
+                int position = viewHolder.getAdapterPosition();
+                notificationModels.remove(position);
+                notificationAdapter.notifyDataSetChanged();
+
+                AppCompatActivity activity = (AppCompatActivity) view.getContext();
+
+                SharedPreferences sharedPreferences = activity.getSharedPreferences(MyPREFERENCES, activity.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                Gson gson = new Gson();
+                String json1 = gson.toJson(notificationModels);
+                editor.putString("models", json1);
+                editor.apply();
+
+            }
+        };
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        itemTouchHelper.attachToRecyclerView(recyclerView);
+
+        recyclerView.setAdapter(notificationAdapter);
 
         return view;
     }
+
 
 }
